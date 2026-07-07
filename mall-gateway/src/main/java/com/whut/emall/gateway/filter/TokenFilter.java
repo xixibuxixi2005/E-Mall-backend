4f59db8b646af5e8a955a49243ec4ee36762dcba
IceKSener <1878024009@qq.com>
2026-07-07 12:14:05 +0800

    feat: 用户信息写入请求头

--- mall-gateway/src/main/java/com/whut/emall/gateway/filter/TokenFilter.java
+++ mall-gateway/src/main/java/com/whut/emall/gateway/filter/TokenFilter.java
@@ -1,14 +1,24 @@
 package com.whut.emall.gateway.filter;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ServerWebExchange;
 
+import com.whut.emall.common.entitiy.JwtPayload;
+import com.whut.emall.common.utils.JwtUtils;
+
+import jakarta.annotation.Resource;
 import reactor.core.publisher.Mono;
 
 public class TokenFilter implements GlobalFilter,Ordered{
+    @Resource JwtUtils jwtUtils;
+    Logger logger = LoggerFactory.getLogger(getClass());
+
     private final String[] publicPrefix = {
         "/api/v1/auth"
     };
@@ -30,14 +40,23 @@ public class TokenFilter implements GlobalFilter,Ordered{
             return chain.filter(exchange);
         
         String token = request.getHeaders().getFirst("Authorization");
-        if (token == null || token.isEmpty()) {
+        if (token == null || token.isEmpty() || !token.startsWith("Bearer ")) {
+            response.setStatusCode(HttpStatus.UNAUTHORIZED);
+            return response.setComplete();
+        }
+
+        JwtPayload payload;
+        try {
+            payload = jwtUtils.verify(token.substring(7));
+        } catch (Exception e) {
+            logger.error(e.getLocalizedMessage());
             response.setStatusCode(HttpStatus.UNAUTHORIZED);
             return response.setComplete();
         }
 
         request = request.mutate()
-                    .header("X-User-Id", "114514")
-                    .header("X-Username", "User114")
+                    .header("X-User-Id", ""+payload.getUserId())
+                    .header("X-Username", payload.getUsername())
                     .build();
 
         exchange.mutate().request(request).build();
