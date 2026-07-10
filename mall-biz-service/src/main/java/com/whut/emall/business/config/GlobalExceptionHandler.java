1825776ac4efc4b581a12b7086a5104fa58f109e
IceKSener <1878024009@qq.com>
2026-07-10 10:00:51 +0800

    fix: 修复请求错误路径不返回404的错误

--- mall-biz-service/src/main/java/com/whut/emall/business/config/GlobalExceptionHandler.java
+++ mall-biz-service/src/main/java/com/whut/emall/business/config/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import org.springframework.validation.BindException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import com.whut.emall.common.entity.ApiException;
 import com.whut.emall.common.entity.ApiResult;
@@ -31,6 +32,11 @@ public class GlobalExceptionHandler{
     @ExceptionHandler(ConstraintViolationException.class)
     public ApiResult handleValidationException(ConstraintViolationException err) {
         return ApiException.err(400, "请求参数校验失败: " + err.getConstraintViolations().iterator().next().getMessage()).toResult();
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ApiResult handleNoResourceFoundException(NoResourceFoundException err) {
+        return ApiException.err(404, "无效的请求路径: " + err.getResourcePath()).toResult();
     }
 
     @ExceptionHandler(Exception.class)
